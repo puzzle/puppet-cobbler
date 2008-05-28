4a4d462348964d26e895c7889c7a71e0fa5c822b
haerry@puzzle.ch <mhaerry@pw-mhaerry.puzzle.itc>
2008-05-29 00:04:20 +0200

    added config files

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,9 @@
 # General Public License version 3 as published by 
 # the Free Software Foundation.
 #
+#
+# Variables to set:
+# - $cobbler_auth_conf_pwd => your password to the auth_config
 
 # modules_dir { \"cobbler\": }
 
@@ -36,6 +39,54 @@ class cobbler::base {
 
     file{'/etc/cron.daily/cobbler_reposync':
         source => "puppet://$server/cobbler/cron/cobbler_reposync",
+        owner => root, group => 0, mode => 0644;
+    }
+
+    file{'/etc/httpd/conf.d/cobbler.conf':
+        source => [ "puppet://$server/files/cobbler/httpd/${fqdn}/cobbler.conf",
+                    "puppet://$server/files/cobbler/httpd/cobbler.conf",
+                    "puppet://$server/cobbler/httpd/cobbler.conf" ],
+        require => Package[apache],
+        notify => Service[apache],
+        owner => root, group => 0, mode => 0644;
+    }
+    
+    file{'/etc/httpd/conf.d/cobbler_svc.conf':
+        source => [ "puppet://$server/files/cobbler/httpd/${fqdn}/cobbler_svc.conf",
+                    "puppet://$server/files/cobbler/httpd/cobbler_svc.conf",
+                    "puppet://$server/cobbler/httpd/cobbler_svc.conf" ],
+        require => Package[apache],
+        notify => Service[apache],
+        owner => root, group => 0, mode => 0644;
+    }
+
+    case $cobbler_auth_conf_pwd {
+        '': { fail("You need to define the cobbler_auth_conf_pwd variable to set a password!") }
+    }
+    file{'/etc/cobbler/auth.conf':
+        content => template("cobbler/auth.conf.erb"),
+        require => Package[cobbler],
+        notify => Service[cobblerd],
+        owner => root, group => 0, mode => 0644;
+    }  
+
+    # deploy all config files
+    cobbler::etconfig{  [ "default.ks", "dhcp.template", "dnsmasq.template", "legacy.ks", 
+                          "modules.conf", "named.template", "pxedefault.template", 
+                          "pxeprofile.template", "pxesystem_ia64.template", 
+                          "pxesystem.template", "rsync.exclude", "sample_end.ks", "sample.ks", 
+                          "settings", "users.conf", "users.digest", "webui-cherrypy.cfg", 
+                          "zone.template" ] : 
+    } 
+}
+
+define cobbler::etcconfig(){
+    file{"/etc/cobbler/${name}":
+        source => [ "puppet://$server/files/cobbler/etc/${fqdn}/${name}"
+                    "puppet://$server/files/cobbler/etc/${name}",
+                    "puppet://$server/cobbler/etc/${name}" ],
+        require => Package[cobbler],
+        notify => Service[cobblerd],
         owner => root, group => 0, mode => 0644;
     }
 }
