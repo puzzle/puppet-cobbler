a2844a149839e6179bb96dff3736af74a6df01ef
Marcel Haerry <haerry@puzzle.ch>
2010-09-27 11:15:08 +0200

    readd code that was lost during c&p

--- manifests/base.pp
+++ manifests/base.pp
@@ -47,4 +47,45 @@ class cobbler::base {
     notify => Exec['cobbler_sync'],
     owner => root, group => 0, mode => 0755;
   }
+
+  cobbler::etcconfig{  [ 'default.ks', 'dhcp.template', 'dnsmasq.template',
+                         'modules.conf', 'named.template', 'tftpd-rules.template',
+                         'rsync.exclude', 'settings', 'users.conf', 'users.digest',
+                         'webui-cherrypy.cfg', 'zone.template', 'cheetah_macros', 'acls.conf',
+                         # pxe/
+                         'pxe/pxedefault.template',  'pxe/pxelocal.template',
+                         'pxe/pxesystem_ppc.template', 'pxe/pxesystem_s390x.template',
+                         'pxe/pxeprofile.template', 'pxe/pxesystem_ia64.template',
+                         'pxe/pxesystem.template',
+                         # power/
+                         'power/power_apc_snmp.template', 'power/power_bladecenter.template',
+                         'power/power_bullpap.template', 'power/power_drac.template',
+                         'power/power_ether_wake.template', 'power/power_ilo.template',
+                         'power/power_ipmilan.template', 'power/power_ipmitool.template',
+                         'power/power_lpar.template', 'power/power_rsa.template',
+                         'power/power_virsh.template', 'power/power_wti.template',
+                         # reporting
+                         'reporting/build_report_email.template' ]:
+  }
+ 
+  cobbler::kickstartfile{ ['default.ks', 'legacy.ks', 'pxerescue.ks', 'sample_end.ks',
+                           'sample.ks', 'sample.seed' ]:
+  }
+ 
+  file{'/var/lib/cobbler/snippets':
+    source => [ "puppet://$server/files/cobbler/${fqdn}/snippets",
+                "puppet://$server/files/cobbler/${cobbler_env}/snippets",
+                "puppet://$server/files/cobbler/snippets/default",
+                "puppet://$server/cobbler/snippets" ],
+    purge => true,
+    recurse => true,
+    notify => [ Exec['cobbler_sync'], Service['cobblerd'] ],
+    owner => root, group => 0, mode => 0644,
+  }
+
+  exec{'cobbler_sync':
+    command => 'cobbler sync',
+    refreshonly => true,
+    require => Package['cobbler', 'genisoimage', 'syslinux'],
+  }
 }
